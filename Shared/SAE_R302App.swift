db6a518d49bd6f901621d9a9ac31178fc438e9f9
Nik97410 <90002518+Nik97410@users.noreply.github.com>
2022-08-24 18:17:58 +0400

    Initial Commit

---
+++ Shared/SAE_R302App.swift
@@ -0,0 +1,17 @@
+//
+//  SAE_R302App.swift
+//  Shared
+//
+//  Created by Marco on 24/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SAE_R302App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
